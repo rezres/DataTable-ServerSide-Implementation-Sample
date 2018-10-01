dbbad7ffc6884a5555c11b71c95324e9222f6707
Munzer Nasr <94munzernasr@gmail.com>
2018-10-01 22:15:16 +0800

    Add project files.

---
+++ DataTable ServerSide  Implementation Sample/Pages/Index.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DataTable_ServerSide__Implementation_Sample.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
